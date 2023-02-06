021ef955ade18de92d3f8cec038d6e98578e9595
MollachievShamil <mollachiev.shamil@yandex.ru>
2023-02-06 20:53:26 +0700

    поменян метод сохранения картинки

--- GroceryList/Services/Networking/Registration/RegistrationResponse.swift
+++ GroceryList/Services/Networking/Registration/RegistrationResponse.swift
@@ -16,11 +16,12 @@ struct RegistrationResponse: Codable {
 struct User: Codable {
     var id: Int?
     var userName: String?
-    var avatar: Data?
+    var avatar: String?
     var email: String?
     var token: String?
     var isConfirmed: Bool?
     var password: String?
+    var avatarAsData: Data?
     
     enum CodingKeys: String, CodingKey {
         case id, userName, avatar, email, token, isConfirmed = "is_confirmed", password
